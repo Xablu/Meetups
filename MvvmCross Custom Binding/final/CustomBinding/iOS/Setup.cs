6d32a5b830b9db83d2b60a036dd8235603d8fcab
Maurits van Beusekom <mvanbeusekom@route2it.com>
2017-02-10 13:31:51 +0100

    Add “MvvmCross Custom Binding” presentation

---
+++ MvvmCross Custom Binding/final/CustomBinding/iOS/Setup.cs
@@ -0,0 +1,41 @@
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Platform.Platform;
+using UIKit;
+
+namespace CustomBinding.iOS
+{
+    public class Setup : MvxIosSetup
+    {
+        public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+        
+        public Setup(MvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter)
+            : base(applicationDelegate, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+        
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+
+		protected override void FillTargetFactories(MvvmCross.Binding.Bindings.Target.Construction.IMvxTargetBindingFactoryRegistry registry)
+		{
+			registry.RegisterCustomBindingFactory<BinaryEdit>(
+				"MyCount", 
+				(arg) => new BinaryEditTargetBinding(arg));
+			
+			base.FillTargetFactories(registry);
+		}
+    }
+}
